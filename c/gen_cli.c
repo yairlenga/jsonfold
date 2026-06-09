bc4b564efbcd00c93bc867d884de1c25f1fd1ad7
Yair Lenga <yair.lenga@gmail.com>
2026-06-09 08:58:13 +0300

    Add "demo" mode to the generated C program

--- c/gen_cli.c
+++ c/gen_cli.c
@@ -1,9 +1,39 @@
-#include "generated.h"
+#include <stdio.h>
+#include <string.h>
 
-#include <stdlib.h>
+#include "jsonfold.h"
 
-int main(int argc, char **argv)
+static const char DEMO_JSON[] =
+"{\n"
+"  \"a\": [\n"
+"    1,\n"
+"    2,\n"
+"    3\n"
+"  ],\n"
+"  \"b\": {\n"
+"    \"x\": true,\n"
+"    \"y\": false\n"
+"  }\n"
+"}\n";
+
+int main(void)
 {
-    printf("%s: NYI\n", argv[0]) ;
-    exit(EXIT_FAILURE) ;
+    jsonfold_writer *w = jsonfold_file_writer_new(
+        stdout,
+        &JSONFOLD_CONFIG_DEFAULT);
+
+    if (!w) {
+        fprintf(stderr, "jsonfold: failed to create writer\n");
+        return 1;
+    }
+
+    if (jsonfold_write(w, DEMO_JSON, strlen(DEMO_JSON)) < 0 ||
+        jsonfold_finish(w) != 0) {
+        fprintf(stderr, "jsonfold: write failed\n");
+        jsonfold_writer_free(w);
+        return 1;
+    }
+
+    jsonfold_writer_free(w);
+    return 0;
 }
